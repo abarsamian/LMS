f2e48a3991f9c178d64296f6f2b3017ee68c1e6b
Anastasia Barsamian <abarsamian@mail.valenciacollege.edu>
2026-02-06 22:51:15 -0500

    finished main and tested

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,41 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome to your Library Management System!!");
-        System.out.println("Please choose one:");
-        System.out.println("1. Add patron");
-        System.out.println("2. Remove patron");
-        System.out.println("3. Edit patron");
-        System.out.println("4. Show all patrons");
-        System.out.println("5. Load patron from file");
-
+        Library lib = new Library();
         Scanner scnr = new Scanner(System.in);
-        int choice = scnr.nextInt();
+
+        while (true) {
+            System.out.println("Welcome to your Library Management System!!");
+            System.out.println("Please choose one:");
+            System.out.println("1. Add patron");
+            System.out.println("2. Remove patron");
+            System.out.println("3. Edit patron");
+            System.out.println("4. Show all patrons");
+            System.out.println("5. Load patron from file");
+            System.out.println("6. Exit");
 
 
+            int choice = scnr.nextInt();
+
+
+            if (choice == 1) {
+                lib.addPatron();
+            } else if (choice == 2) {
+                lib.removePatron();
+            } else if (choice == 3) {
+                lib.editPatron();
+            } else if (choice == 4) {
+                lib.showAllPatrons();
+            } else if (choice == 5) {
+                lib.loadFromFile();
+            } else if (choice == 6) {
+                break;
+            } else {
+                System.out.println("Invalid entry.");
+            }
+        }
     }
 }
+
+
+
